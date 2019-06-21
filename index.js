3d1ebba3418b8d3a6011fcbd3ef1c561e119183f
Annie Phan <anniephan.atp@gmail.com>
2019-06-21 12:33:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, driverRevenue) {
+  return drivers.filter( function (driver) {
+    return driver.revenue > driverRevenue;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, driverRevenue) {
+  return driversWithRevenueOver(drivers, driverRevenue)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
+
+function exactMatch(drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+
+  });
+}
+
+function exactMatchToList(drivers, matcher) {
+  return exactMatch(drivers, matcher)
+  .map(function (driver) {
+    return driver.name
+  })
+}
